f9702aa09fbb96a623f5c3399ba701c2db62864a
Neccolini <shun11202991@gmail.com>
2020-11-25 23:48:57 +0900

    fixed

--- Assets/Scenes/Scripts/Goal2.cs
+++ Assets/Scenes/Scripts/Goal2.cs
@@ -3,33 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-public class leftout : MonoBehaviour
+public class Goal2 : MonoBehaviour
 {
-
-    public string Score2Text; // 表示する文字列 Player2: x点
-    public static int Player2Score=0; // 点数
-    const int EndCount=3; // 終了する点数 <-ここを変える
+    public string Score1Text; // 表示する文字列 Player1: x点
+    public static int Player1Score = 0; // 点数
+    const int EndCount=3; //終了する点数 <-ここを変える
     GameObject puck; //パック
     GameObject targetText; //テキストオブジェクト
     private bool isGoal=false;
     void Start()
     {
-        Player2Score=0;
+        Player1Score=0;
         puck=GameObject.Find("puck");
-        targetText=GameObject.Find("LeftText");
+        targetText=GameObject.Find("RightText");
     }
     void Update()
     {
         //Player1側のゴールにパックが入ったら
-        if(puck.transform.position.x<-10.0f && isGoal==false){
-            Player2Score++;
-            Score2Text= ("Player2: " + Player2Score + "点");
-            targetText.GetComponent<Text>().text=Score2Text;
+        if(puck.transform.position.x>10.0f && isGoal==false){
+            Player1Score++;
+            Score1Text= ("Player1:" + Player1Score + "点");
+            targetText.GetComponent<Text>().text=Score1Text;
             isGoal=true;
             //ゲーム終了
-            if(Player2Score>=EndCount){
-                targetText.GetComponent<Text>().text="You win ! ";
+            if(Player1Score>=EndCount){
+                targetText.GetComponent<Text>().text="You lose...";
                 Debug.Log("Quit");
                 QuitClass q=new QuitClass();
                 q.Quit();
@@ -37,12 +35,11 @@ public class leftout : MonoBehaviour
             Invoke("ResetPuck", 3.5f);
         }
     }
-
     //パックをリセット
     private void ResetPuck()
     {
         puck.transform.position=new Vector3(0.0f,0.0f,0.0f);
-        puck.transform.eulerAngles=new Vector3(0, Random.Range(0,360),0);
+        puck.transform.eulerAngles= new Vector3(0, Random.Range(0,360),0);
         puck.GetComponent<Rigidbody>().velocity/=10.0f;
         isGoal=false;
     }
